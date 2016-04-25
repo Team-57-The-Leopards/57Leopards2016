b855796d5bdba77a52ce49a09e8cace9937f20a9
Joshua Bryant <jbryant57.2587@gmail.com>
2016-04-25 01:15:26 -0500

    Adding code for swithing auton routines. Forgot to add the code in the last commit.

--- src/Commands/AutonomousCommand.cpp
+++ src/Commands/AutonomousCommand.cpp
@@ -11,8 +11,7 @@ AutonomousCommand::AutonomousCommand(){
 
 AutonomousCommand::AutonomousCommand(int choice)
 {
-	LowBar();
-	/*switch(choice){
+	switch(choice){
 		case 0:
 			LowBar();
 			break;
@@ -20,27 +19,11 @@ AutonomousCommand::AutonomousCommand(int choice)
 			Moat();
 			break;
 		case 2:
-			//RockWall();
+			RockWall();
 			break;
 		default:
 			break;
-	}*/
-	// Add Commands here:
-	// e.g. AddSequential(new Command1());
-	//      AddSequential(new Command2());
-	// these will run in order.
-
-	// To run multiple commands at the same time,
-	// use AddParallel()
-	// e.g. AddParallel(new Command1());
-	//      AddSequential(new Command2());
-	// Command1 and Command2 will run in parallel.
-
-	// A command group will require all of the subsystems that each member
-	// would require.
-	// e.g. if Command1 requires chassis, and Command2 requires arm,
-	// a CommandGroup containing them would require both the chassis and the
-	// arm.
+	}
 }
 
 void AutonomousCommand::LowBar(){
@@ -48,16 +31,16 @@ void AutonomousCommand::LowBar(){
 	AddSequential(new Roller_In(0.75, 0.5));//Intake preload ball
 	AddParallel(new Roller_Stop());
 	AddSequential(new AutonomousDrive(-0.75,0,4));//Drive Forward, full speed over defense
-	AddSequential(new AutonomousDrive(0,0,0.5));//stop after 3 seconds
+	AddSequential(new AutonomousDrive(0,0,0.5));//stop after 4 seconds
 	AddSequential(new Intake_Pos(-24661));
 }
-`
+
 void AutonomousCommand::RockWall(){
 	AddParallel(new Intake_Pos(-125865));//set intake to horizontal position while intaking pre-load ball
 	AddSequential(new Roller_In(0.75, 0.5));//Intake preload ball
 	AddParallel(new Roller_Stop());
 	AddSequential(new AutonomousDrive(-0.5,0,4.5));//Drive Forward, full speed over defense
-	AddSequential(new AutonomousDrive(0,0,0.5));//stop after 3 seconds
+	AddSequential(new AutonomousDrive(0,0,0.5));//stop after 4.5 seconds
 	AddSequential(new Intake_Pos(-24661));
 }
 
@@ -66,7 +49,6 @@ void AutonomousCommand::Moat(){
 	AddSequential(new Roller_In(0.75, 0.5));//Intake preload ball
 	AddParallel(new Roller_Stop());
 	AddSequential(new AutonomousDrive(-1,0,2.5));//Drive Forward, full speed over defense
-	AddSequential(new AutonomousDrive(0,0,0.5));//stop after 3 seconds
+	AddSequential(new AutonomousDrive(0,0,0.5));//stop after 2.5 seconds
 	AddSequential(new Intake_Pos(-24661));
 }
-
